9e4205f4354c351648666fecaedf16fe21fa64c9
girvel <widauka@ya.ru>
2021-10-29 23:52:10 +0500

    It finally does something!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser};
-use std::fs::{create_dir_all, File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io;
+use std::io::Write;
 use std::path::Path;
 use std::process::exit;
 
@@ -21,25 +22,33 @@ struct Add {
     what: String,
 }
 
+fn generate_name() -> String {String::from("task")}
+
 fn main() {
     let opts: Opts = Opts::parse();
 
     match opts.subcommand {
         SubCommand::Add(add) => {
-            let path_to_task = Path::new("/var/rb/task");
-            create_dir_all(path_to_task.parent().expect(""))
-                .expect("Can not create data directory for routine-bender");
+            // TODO in home directory
+            let mut path_to_task: &Path;
+            // create_dir_all(path_to_task.parent().expect(""))
+            //     .expect("Can not create data directory for routine-bender");
 
             let mut file = loop {
+                let name = generate_name();
+                path_to_task = Path::new(&name);
                 match OpenOptions::new().write(true).create_new(true).open(path_to_task) {
                     Ok(file) => break file,
-                    Err(io::Error::) => {}
-                    _ => { println!("Can not create file for task, sorry."); exit(1) },
-                }
+                    Err(e) => {
+                        if e.kind() != io::ErrorKind::AlreadyExists {
+                            println!("Something went wrong during creation of a file\n{:?}", e);
+                            exit(1);
+                        }
+                    },
+                };
             };
 
-            let mut file = File::create("/var/rb/task").expect("Can not open file");
-            file.write_all(add.what.as_bytes()).expect("Can not write to file");
+            file.write_all(add.what.as_bytes());
         },
     };
 }
